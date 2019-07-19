a8ac839a621ea35ca30c8a97c0216ce5170ea075
chesterven <vd16006@ues.edu.sv>
2019-07-18 21:23:19 -0600

    imagen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    Página principal
+                <img src="/img/minerva-sf.png" class="rounded float-left" width="40dp"  alt="imagen inicial">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
